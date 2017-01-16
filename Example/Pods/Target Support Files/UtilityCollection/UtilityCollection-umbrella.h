a283b334f47a3da0ed8f89c50fc7be5c2c7ea18f
Naresh Kukkala <nareshk@Nareshs-MacBook-Pro.local>
2017-01-16 18:42:30 +0530

    add pod config

--- Example/Pods/Target Support Files/UtilityCollection/UtilityCollection-umbrella.h
+++ Example/Pods/Target Support Files/UtilityCollection/UtilityCollection-umbrella.h
@@ -2,6 +2,22 @@
 #import <UIKit/UIKit.h>
 #endif
 
+#import "SystemServices.h"
+#import "route.h"
+#import "SSAccelerometerInfo.h"
+#import "SSAccessoryInfo.h"
+#import "SSApplicationInfo.h"
+#import "SSBatteryInfo.h"
+#import "SSCarrierInfo.h"
+#import "SSDiskInfo.h"
+#import "SSHardwareInfo.h"
+#import "SSJailbreakCheck.h"
+#import "SSLocalizationInfo.h"
+#import "SSMemoryInfo.h"
+#import "SSNetworkInfo.h"
+#import "SSProcessInfo.h"
+#import "SSProcessorInfo.h"
+#import "SSUUID.h"
 
 FOUNDATION_EXPORT double UtilityCollectionVersionNumber;
 FOUNDATION_EXPORT const unsigned char UtilityCollectionVersionString[];
